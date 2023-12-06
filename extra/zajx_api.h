bd87060815e3904175276d01924b533495b5bf85
jinyq <jinyq7@163.com>
2023-12-06 16:17:45 +0800

    Update zajx_api.h
    change zajx_api to project_api

--- extra/zajx_api.h
+++ extra/zajx_api.h
@@ -1,17 +1,17 @@
-#ifdef ZAJX_API_EXPORTS
+#ifdef PROJECT_API_EXPORTS
 
 #if defined(_MSC_VER)
-#define ZAJX_API __declspec(dllexport) 
+#define PROJECT_API __declspec(dllexport) 
 #else
-#define ZAJX_API __attribute__((visibility("default")))
+#define PROJECT_API __attribute__((visibility("default")))
 #endif
 
 #else
 	
 #if defined(_MSC_VER)
-#define ZAJX_API __declspec(dllimport) 
+#define PROJECT_API __declspec(dllimport) 
 #else
-#define ZAJX_API
+#define PROJECT_API
 #endif
 
 #endif
